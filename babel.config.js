c9cd82eaec0b808b499e71698d3a81a0d9c630a7
Henrique Menezes <82477364+menezesho@users.noreply.github.com>
2024-09-12 10:15:00 -0300

    fix - correção de versionamento de packages

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@assets': './src/assets',
+            '@config': './src/config',
+            '@utils': './src/utils',
+            '@hooks': './src/hooks',
+            '@services': './src/services',
+            '@context': './src/context',
+          },
+        },
+      ],
+    ],
   };
 };
