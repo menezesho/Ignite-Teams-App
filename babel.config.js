6cce5af191db04db6e3c9cd63005dea31ca37208
Henrique Menezes <henryyquemenezes@hotmail.com>
2024-09-30 10:21:39 -0300

    feat - configuração do atalho do diretório storage

--- babel.config.js
+++ babel.config.js
@@ -16,6 +16,7 @@ module.exports = function (api) {
             '@hooks': './src/hooks',
             '@services': './src/services',
             '@context': './src/context',
+            '@storage': './src/storage',
           },
         },
       ],
